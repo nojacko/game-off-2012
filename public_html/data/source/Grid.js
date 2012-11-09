6df38c706dada09cecb30a636b031b66594c75bb
James Jackson <git@nojacko.com>
2012-11-09 13:40:53 +0000

    Convert grid to format used by dijkstras-js

--- public_html/data/source/Grid.js
+++ public_html/data/source/Grid.js
@@ -3,8 +3,11 @@
 	function Grid (map, grid) 
 	{
 		this.map = map;
-		this.grid = grid;
-		this.graph = new Graph(this.grid);
+		this.layout = grid;		
+		this.graph = new Graph(this.layout);
+		
+		this.dijkstras = new Dijkstras();
+		this.dijkstras.setGraph(this.gridToGraph(this.layout));
 	}
 	
 	// Properties
@@ -12,7 +15,54 @@
 	Grid.prototype.grid			= null;
 	Grid.prototype.graph		= null;
 	
-	// Methods		
+	// Methods	
+	Grid.prototype.gridToGraph = function (grid) 
+	{
+		// List of offset to check when creating vertices [x, y]
+		var offsets = [
+			[-1, -1], [0, -1], [1, -1],
+			[-1,  0], /* N */ [1,  0],
+			[-1,  1], [0,  1], [1,  1]
+		];
+		
+		var graph = [];
+		
+		for (var y = 0; y < grid.length; y++) {
+			var row = grid[y];
+			for (var x = 0; x < row.length; x++) {
+				var cell = row[x];
+				var nodeName = x + 'x' + y;
+				
+				// Inaccessible
+				if (cell === 0) {
+					continue;
+				}				
+				
+				// Check neigbours
+				var vertices = [];
+				for (var n in offsets) {
+					var xOffset = x - offsets[n][0];
+					var yOffset = y - offsets[n][1];
+					
+					if (typeof grid[yOffset] !== 'undefined') {	
+						if (typeof grid[yOffset][xOffset] !== 'undefined') {	
+							var targetName = xOffset + 'x' + yOffset;
+							
+							// Add vertex
+							vertices[vertices.length] = [
+								targetName, 
+								Math.distance2D(x, y, xOffset, yOffset)
+							]; 
+						}
+					}
+				}
+				
+				graph[graph.length] = [nodeName, vertices];
+			}
+		}		
+		return graph;
+	}
+		
 	Grid.prototype.findPath = function (from, to) 
 	{
 		return astar.search(
