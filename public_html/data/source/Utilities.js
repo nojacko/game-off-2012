afadebdca644c88bf7ec19f60fc4d6f6774a29dd
James Jackson <git@nojacko.com>
2012-11-27 23:33:29 +0000

    Functions for converting hex to rgb and back

--- public_html/data/source/Utilities.js
+++ public_html/data/source/Utilities.js
@@ -118,3 +118,30 @@ if (!Array.prototype.prototype) {
 		}
 	});
 }
+
+/**
+ * Convert HEX to RGB
+ * http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+ */
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+
+/**
+ * Convert RGB to HEX
+ * http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+ */
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
